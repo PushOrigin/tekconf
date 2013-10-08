bfb24f59d1042b382da0a7ee26da8940731a03fa
Rob Gibbens <RobGibbens@gmail.com>
2013-10-08 15:45:43 -0400

    UberImporter

---
+++ Importer/UberImporter/Importers/Cododn2013Importer.cs
@@ -0,0 +1,111 @@
+﻿namespace UberImporter.Importers.MonkeySpace2012
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+
+    using AutoMapper;
+
+    using MongoDB.Driver.Linq;
+
+    using ServiceStack.Text;
+
+    using TekConf.Common.Entities;
+
+    using TinyMessenger;
+
+    public class Cododn2013Importer
+    {
+        public void Import()
+        {
+            var connection = new MongoDbConnection();
+            var collection = connection.RemoteDatabase.GetCollection<ConferenceEntity>("conferences");
+            var sessionsUrl = "http://cododn.azurewebsites.net/api/sessions";
+            var speakersUrl = "http://cododn.azurewebsites.net/api/presenters";
+            var request = new WebClient();
+            var sessionsJson = request.DownloadString(sessionsUrl);
+            var speakersJson = request.DownloadString(speakersUrl);
+            var sessionsRoot = sessionsJson.FromJson<SessionRoot>();
+            var speakersRoot = speakersJson.FromJson<SpeakerRoot>();
+
+            if (sessionsRoot != null)
+            {
+                var conference = collection.AsQueryable().SingleOrDefault(c => c.slug == "central-ohio-day-of-net-2013");
+                if (conference == null)
+                {
+                    ITinyMessengerHub hub = new TinyMessengerHub();
+                    IEntityConfiguration configuration = new EntityConfiguration();
+                    var repository = new ConferenceRepository(configuration);
+                    conference = new ConferenceEntity(hub, repository) { };
+                }
+                else
+                {
+                    foreach (var session in sessionsRoot.Sessions)
+                    {
+                        var sessionEntity =
+                            conference.sessions.SingleOrDefault(s => s.slug == session.Title.GenerateSlug());
+                        if (sessionEntity == null)
+                        {
+                            sessionEntity = Mapper.Map<SessionEntity>(session);
+                            if (sessionEntity.start == default(DateTime))
+                            {
+                                if (conference.start != null)
+                                {
+                                    sessionEntity.start = conference.start.Value;
+                                }
+                            }
+                            if (sessionEntity.end == default(DateTime))
+                            {
+                                if (conference.start != null)
+                                {
+                                    sessionEntity.end = conference.start.Value.AddHours(1);
+                                }
+                            }
+                            sessionEntity.slug = session.Title.GenerateSlug();
+                            conference.AddSession(sessionEntity);
+                        }
+                        sessionEntity.slug = session.Title.GenerateSlug();
+                        var speakerJson = speakersRoot.Speakers.SingleOrDefault(s => s.SpeakerUrl == session.SpeakerUrl);
+                        var speakerEntity = Mapper.Map<SpeakerEntity>(speakerJson);
+                        speakerEntity.slug = speakerEntity.fullName.ToLower().GenerateSlug();
+                        if (sessionEntity.speakers.All(x => x.slug != speakerEntity.slug))
+                        {
+                            conference.AddSpeakerToSession(sessionEntity.slug, speakerEntity);
+                        }
+                    }
+                }
+                conference.Save();
+            }
+        }
+
+        public class SessionRoot
+        {
+            public Session[] Sessions { get; set; }
+        }
+
+        public class Session
+        {
+            public string Id { get; set; }
+            public string Title { get; set; }
+            public string Abstract { get; set; }
+            public object Room { get; set; }
+            public string[] Tags { get; set; }
+            public string SessionUrl { get; set; }
+            public string SpeakerUrl { get; set; }
+        }
+
+        public class SpeakerRoot
+        {
+            public Speaker[] Speakers { get; set; }
+        }
+
+        public class Speaker
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Bio { get; set; }
+            public string SpeakerUrl { get; set; }
+            public string[] SessionUrls { get; set; }
+        }
+    }
+}
--- Importer/UberImporter/Program.cs
+++ Importer/UberImporter/Program.cs
@@ -2,10 +2,15 @@
 
 namespace UberImporter
 {
+    using AutoMapper;
+
+    using TekConf.Common.Entities;
+
     class Program
     {
         static void Main(string[] args)
         {
+            BootstrapAutoMapper();
             //MongoDbConnection connection = new MongoDbConnection();
             //var collection = connection.RemoteDatabase.GetCollection<ConferenceEntity>("conferences");
 
@@ -25,19 +30,40 @@ namespace UberImporter
             //    collection.Remove(query2);
             //}
 
-            
 
-            var monkeySpace = new MonkeySpaceImporter();
-						//var railsConf = new RailsConfImporter();
-						//var codestock = new CodeStockImporter();
-						//var oneDevDayDetroit = new OneDevDayDetroitImporter();
-						//var codeMash2013 = new CodeMash2013Importer();
+            var cododn2013 = new Cododn2013Importer();
+            //var monkeySpace = new MonkeySpaceImporter();
+            //var railsConf = new RailsConfImporter();
+            //var codestock = new CodeStockImporter();
+            //var oneDevDayDetroit = new OneDevDayDetroitImporter();
+            //var codeMash2013 = new CodeMash2013Importer();
 
             //oneDevDayDetroit.Import();
-            monkeySpace.Import();
+            cododn2013.Import();
             //railsConf.Import();
             //codestock.Import();
             //codeMash2013.Import();
         }
+
+        private static void BootstrapAutoMapper()
+        {
+            Mapper.CreateMap<Cododn2013Importer.Session, SessionEntity>()
+                .ForMember(x => x.description, opt => opt.MapFrom(s => s.Abstract))
+                .ForMember(x => x.room, opt => opt.MapFrom(s => s.Room))
+                .ForMember(x => x.title, opt => opt.MapFrom(s => s.Title))
+                .ForMember(x => x._id, opt => opt.Ignore())
+                ;
+
+            Mapper.CreateMap<Cododn2013Importer.Speaker, SpeakerEntity>()
+                .ForMember(x => x.description, opt => opt.MapFrom(s => s.Bio))
+                .ForMember(x => x.firstName, opt => opt.MapFrom(s => s.Name.Split(' ')[0]))
+                .ForMember(x => x.lastName, opt => opt.MapFrom(s => s.Name.Split(' ')[1]))
+                .ForMember(x => x._id, opt => opt.Ignore())
+                ;
+        }
     }
+
+
+
+
 }
